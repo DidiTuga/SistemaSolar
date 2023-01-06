28763f01570dc2486f7f0de521b3e395dd692964
DidiTuga <diogo.diogosnts.santos2@gmail.com>
2023-01-06 02:57:40 +0000

    Orbita ja da(menos da lua) velocidades corrigidas

--- GLFWOpenGL/include/camera.h
+++ GLFWOpenGL/include/camera.h
@@ -16,11 +16,11 @@ enum Camera_Movement {
 };
 
 // Default camera values
-const float YAW         = -90.0f;
-const float PITCH       =  0.0f;
-const float SPEED       =  2.5f;
-const float SENSITIVITY =  0.1f;
-const float ZOOM        =  45.0f;
+const float YAW = 90.0f;
+const float PITCH = -40.0f;
+const float SPEED = 10.0f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 45.0f;
 
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
@@ -86,7 +86,7 @@ public:
         xoffset *= MouseSensitivity;
         yoffset *= MouseSensitivity;
 
-        Yaw   += xoffset;
+        Yaw += xoffset;
         Pitch += yoffset;
 
         // make sure that when pitch is out of bounds, screen doesn't get flipped
@@ -124,7 +124,7 @@ private:
         Front = glm::normalize(front);
         // also re-calculate the Right and Up vector
         Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        Up    = glm::normalize(glm::cross(Right, Front));
+        Up = glm::normalize(glm::cross(Right, Front));
     }
 };
 #endif
--- GLFWOpenGL/main.cpp
+++ GLFWOpenGL/main.cpp
@@ -9,7 +9,9 @@
 #include <iostream> 
 #include <vector> 
 using namespace std;
+
 #include <math.h>
+#define _USE_MATH_DEFINES
 
 // includes
 #include <objloader.hpp>
@@ -22,19 +24,12 @@ using namespace std;
 #define BUFFER_OFFSET(offset) ((void*)(offset))
 #define MEMBER_OFFSET(s,m) ((char*)NULL + (offsetof(s,m)))
 
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow* window);
-
 // settings
 const unsigned int SCR_WIDTH = 1024;
 const unsigned int SCR_HEIGHT = 768;
 
 // camera
-Camera camera(glm::vec3(-10.0f, 20.0f, -30.0f));
-
+Camera camera(glm::vec3(0.0f, 60.0f, -110.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -43,9 +38,12 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+// variaveis planetas
+glm::vec3 posicao_centro = glm::vec3(0.0f, 0.0f, 0.0f);
+float plan_velocidade = 0.1f;
 
 // lighting
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
 float acc1 = 0.2f;
 float acc2 = 0.5f;
 float acc3 = 1.0f;
@@ -62,9 +60,15 @@ float acc3 = 1.0f;
 #define urano_path "C:/Users/diogo/OneDrive - Universidade da Beira Interior/3ª ano/1º Semestre/Computacao Grafica/Projeto/Ex1/GLFWOpenGL/Textures/2k_uranus.jpg"
 #define neptuno_path "C:/Users/diogo/OneDrive - Universidade da Beira Interior/3ª ano/1º Semestre/Computacao Grafica/Projeto/Ex1/GLFWOpenGL/Textures/2k_neptune.jpg"
 
+
+// funções
 unsigned int loadTexture(const char* path);
 unsigned int loadCubemap(vector<string> faces);
 GLuint SolidSphere(float radius, int slices, int stacks);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
 
 
 int main()
@@ -72,10 +76,12 @@ int main()
     //Propriedades da esfera, camadas longitudinais e transversais
     int slices = 32;
     int stacks = 32;
-
-
+    
     //numero de pontos
     int numIndicies = (slices * stacks + slices) * 6;
+	
+	//inicializar camera
+	
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -180,12 +186,14 @@ int main()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     
-    
-    GLuint vao = SolidSphere(4, 32, 32);
+    // sphere base
+    GLuint vao = SolidSphere(10, 32, 32);
 
-
+    // Modelo de saturno
     Model ourModel("C:/Users/diogo/OneDrive - Universidade da Beira Interior/3ª ano/1º Semestre/Computacao Grafica/Projeto/Ex1/GLFWOpenGL/Textures/Saturno.obj");
 
+	// guardar a posição dos planetas
+	glm::vec3 posicaoPlanetas[8];
 
 	// LOAD TEXTURES
 	unsigned int sol_tex = loadTexture(sol_path);
@@ -214,6 +222,39 @@ int main()
     skyboxShader.use();
     skyboxShader.setInt("skybox", 0);
 	
+	// GERAR VETORES COM POSIÇÂO DE CADA ORBITA
+    std::vector<float> orbVert;
+    GLfloat xx;
+    GLfloat zz;
+    float angl;
+    for (int i = 0; i < 200; i++)
+    {
+        angl = (float)(M_PI / 2 - i * (M_PI / 100));
+        xx = sin(angl) * 10.0f;
+        zz = cos(angl) * 10.0f;
+        orbVert.push_back(xx);
+        orbVert.push_back(0.0f);
+        orbVert.push_back(zz);
+
+    }
+
+    /* VAO-VBO para as ORBITAS*/
+    GLuint VBO_t, VAO_t;
+    glGenVertexArrays(1, &VAO_t);
+    glGenBuffers(1, &VBO_t);
+    glBindVertexArray(VAO_t);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_t);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * orbVert.size(), orbVert.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+	
+    // inicializar memoria
+	for (int i = 0; i < 8; i++) {
+		posicaoPlanetas[i] = posicao_centro;
+	}
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -249,7 +290,7 @@ int main()
         sphereShader.setVec3("viewPos", camera.Position);
 
         // ########################################## Terra #########################################
-         //Aqui especificamos que VAO queremos que esteja ativo para o render do respetivo planeta
+        //Aqui especificamos que VAO queremos que esteja ativo para o render do respetivo planeta
         glBindVertexArray(vao);
 
         //especificamos que textura queremos que fique ativa
@@ -268,9 +309,12 @@ int main()
         // world transformation
         glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 moon;
+        // posição da terra
+        double xx = sin(glfwGetTime() * plan_velocidade * 0.75f) * 10.0f * 3.0f * 2.0f;
+        double zz = cos(glfwGetTime() * plan_velocidade * 0.75f) * 10.0f * 3.0f * 2.0f;
+        posicaoPlanetas[2] = glm::vec3(xx, 0, zz);
 
-
-        model = glm::rotate(model, (float)glfwGetTime() / 4.5f, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(24.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime() / 3.5f, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, 0.0f, zz));
         moon = model;
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
@@ -279,7 +323,7 @@ int main()
         glDrawElements(GL_TRIANGLES, numIndicies, GL_UNSIGNED_INT, BUFFER_OFFSET(0));
 
 
-        /*###################################### LUA######################################*/
+        //###################################### LUA ######################################
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, lua_tex);
@@ -288,17 +332,18 @@ int main()
 
         // world transformation
         model = glm::mat4(1.0f);
-
-        model = glm::rotate(moon, (float)glfwGetTime(), glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(4.0f, 0.0f, 0.0f));
+		xx = sin(glfwGetTime() * plan_velocidade * 0.55f) * 10.0f * 0.5f * 2.0f;
+		zz = cos(glfwGetTime() * plan_velocidade * 0.55f) * 10.0f * 0.5f * 2.0f;
+        model = glm::rotate(moon, (float)glfwGetTime(), glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, 0.0f, zz));
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(0.1, 0.1, 0.1));
+        model = glm::scale(model, glm::vec3(0.2, 0.2, 0.2));
         sphereShader.setMat4("model", model);
 
         glDrawElements(GL_TRIANGLES, numIndicies, GL_UNSIGNED_INT, BUFFER_OFFSET(0));
 
 
 
-        /*###################################### SOL ######################################*/
+        //###################################### SOL ######################################
         glBindVertexArray(vao);
         glBindTexture(GL_TEXTURE_2D, sol_tex);
 
@@ -310,17 +355,15 @@ int main()
         lightsphereShader.setMat4("view", view);
 
         model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+        model = glm::translate(model, posicao_centro);
         model = glm::rotate(model, (float)glfwGetTime() / 15, glm::vec3(0.0f, 1.0f, 0.0f));
         lightsphereShader.setMat4("model", model);
-
-
 
         glDrawElements(GL_TRIANGLES, numIndicies, GL_UNSIGNED_INT, BUFFER_OFFSET(0));
 		
 
 
-        /*###################################### MERCURIO ######################################*/
+        //###################################### MERCURIO ######################################
 
         glBindVertexArray(vao);
         glActiveTexture(GL_TEXTURE0);
@@ -331,8 +374,10 @@ int main()
 
         // world transformation
         model = glm::mat4(1.0f);
-
-        model = glm::rotate(model, (float)glfwGetTime() / 3, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(8.0f, 0.0f, 0.0f));
+		xx = sin(glfwGetTime() * plan_velocidade) * 10.0f * 1.f * 2.f;
+		zz = cos(glfwGetTime() * plan_velocidade) * 10.0f * 1.f * 2.f;
+		posicaoPlanetas[0] = glm::vec3(xx, 0, zz);
+        model = glm::rotate(model, (float)glfwGetTime() / 3.0f, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, 0.0f, zz));
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
         sphereShader.setMat4("model", model);
@@ -341,7 +386,7 @@ int main()
 
 
 
-        /*###################################### VENUS ######################################*/
+        //###################################### VENUS ######################################
 
         glBindVertexArray(vao);
         glActiveTexture(GL_TEXTURE0);
@@ -351,8 +396,10 @@ int main()
 
         // world transformation
         model = glm::mat4(1.0f);
-
-        model = glm::rotate(model, (float)glfwGetTime() / 4, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(16.0f, 0.0f, 0.0f));
+		xx = sin(glfwGetTime() * plan_velocidade * 0.8f) * 10.0f * 2.f * 2.f;
+		zz = cos(glfwGetTime() * plan_velocidade * 0.8f) * 10.0f * 2.f * 2.f;
+		posicaoPlanetas[1] = glm::vec3(xx, 0, zz);
+        model = glm::rotate(model, (float)glfwGetTime() / 3.25f, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, 0.0f, zz));
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
         sphereShader.setMat4("model", model);
@@ -360,7 +407,7 @@ int main()
         glDrawElements(GL_TRIANGLES, numIndicies, GL_UNSIGNED_INT, BUFFER_OFFSET(0));
 
 
-        /*###################################### MARTE ######################################*/
+        //###################################### MARTE ######################################
 
         glBindVertexArray(vao);
         glActiveTexture(GL_TEXTURE0);
@@ -370,8 +417,11 @@ int main()
 
         // world transformation
         model = glm::mat4(1.0f);
+		xx = sin(glfwGetTime() * plan_velocidade * 0.6f) * 10.0f * 4.f * 2.f;
+		zz = cos(glfwGetTime() * plan_velocidade * 0.6f) * 10.0f * 4.f * 2.f;
+		posicaoPlanetas[3] = glm::vec3(xx, 0, zz);
 
-        model = glm::rotate(model, (float)glfwGetTime() / 4, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(30.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime() / 4, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, 0.0f, zz));
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
         sphereShader.setMat4("model", model);
@@ -379,8 +429,8 @@ int main()
         glDrawElements(GL_TRIANGLES, numIndicies, GL_UNSIGNED_INT, BUFFER_OFFSET(0));
 
 
-        /*###################################### JUPITER ######################################*/
-
+        //###################################### JUPITER ######################################
+       
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, jupiter_tex);
@@ -389,8 +439,10 @@ int main()
 
         // world transformation
         model = glm::mat4(1.0f);
-
-        model = glm::rotate(model, (float)glfwGetTime() / 4, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(40.0f, 0.0f, 0.0f));
+		xx = sin(glfwGetTime() * plan_velocidade * 0.5f) * 10.0f * 5.f * 2.5f;
+		zz = cos(glfwGetTime() * plan_velocidade * 0.5f) * 10.0f * 5.f * 2.5f;
+		posicaoPlanetas[4] = glm::vec3(xx, 0, zz);
+        model = glm::rotate(model, (float)glfwGetTime() / 5, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, 0.0f, zz));
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
         sphereShader.setMat4("model", model);
@@ -398,7 +450,7 @@ int main()
         glDrawElements(GL_TRIANGLES, numIndicies, GL_UNSIGNED_INT, BUFFER_OFFSET(0));
 
 
-        /*###################################### SATURNO ######################################*/
+        //###################################### SATURNO ######################################
 
         glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, saturno_tex);
@@ -407,10 +459,13 @@ int main()
 
         // world transformation
         model = glm::mat4(1.0f);
+		xx = sin(glfwGetTime() * plan_velocidade * 0.48f) * 10.0f * 6.f * 2.5f;
+		zz = cos(glfwGetTime() * plan_velocidade * 0.48f) * 10.0f * 6.f * 2.5f;
+		posicaoPlanetas[5] = glm::vec3(xx, 0, zz);
 
-        model = glm::rotate(model, (float)glfwGetTime() / 6, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(48.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime() / 6, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, .0f, zz));
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));
+        model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
 
         sphereShader.setMat4("model", model);
         ourModel.Draw(sphereShader);
@@ -419,7 +474,7 @@ int main()
 
 
 
-        /*###################################### URANO ######################################*/
+        //###################################### URANOs ######################################
         glBindVertexArray(vao);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, urano_tex);
@@ -428,8 +483,12 @@ int main()
 
         // world transformation
         model = glm::mat4(1.0f);
+		// posição do planeta
+		xx = sin(glfwGetTime() * plan_velocidade * 0.46f) * 10.0f * 7.f * 2.5f;
+		zz = cos(glfwGetTime() * plan_velocidade * 0.46f) * 10.0f * 7.f * 2.5f;
+		posicaoPlanetas[6] = glm::vec3(xx, 0, zz);
 
-        model = glm::rotate(model, (float)glfwGetTime() / 8, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(56.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime() / 7, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, 0.0f, zz));
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
         sphereShader.setMat4("model", model);
@@ -437,7 +496,7 @@ int main()
         glDrawElements(GL_TRIANGLES, numIndicies, GL_UNSIGNED_INT, BUFFER_OFFSET(0));
 
 
-        /*###################################### NEPTUNO ######################################*/
+        //###################################### NEPTUNO ######################################
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, neptuno_tex);
@@ -447,14 +506,50 @@ int main()
 
         // world transformation
         model = glm::mat4(1.0f);
+		// posição do planeta
+		xx = sin(glfwGetTime() * plan_velocidade * 0.44f) * 10.0f * 8.f * 2.5f;
+		zz = cos(glfwGetTime() * plan_velocidade * 0.44f) * 10.0f * 8.f * 2.5f;
+		posicaoPlanetas[7] = glm::vec3(xx, 0, zz);
 
-        model = glm::rotate(model, (float)glfwGetTime() / 10, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(64.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime() / 8, glm::vec3(0.0f, 3.0f, 0.0f)) * glm::translate(model, glm::vec3(xx, 0.0f, zz));
         model = glm::rotate(model, (float)glfwGetTime() + 2, glm::vec3(0.0f, 3.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
         sphereShader.setMat4("model", model);
 
         glDrawElements(GL_TRIANGLES, numIndicies, GL_UNSIGNED_INT, BUFFER_OFFSET(0));
 
+        // ###################################### orbitas ######################################
+        glBindVertexArray(VAO_t);
+
+		// mudar a cor da orbita
+		glm::mat4 modelorb = glm::mat4(1.0f);
+
+
+
+        for (float i = 1; i < 9; i++)
+        {
+            modelorb = glm::mat4(1);
+			// iniciar no meio para fazer o circulo conforme o meio
+			modelorb = glm::translate(modelorb, posicao_centro);
+            if (i <= 4) {
+                modelorb = glm::scale(modelorb, glm::vec3(i * 2.f, i * 2.f, i * 2.f));
+            }
+            else {
+                modelorb = glm::scale(modelorb, glm::vec3(i * 2.5f, i * 2.5f, i * 2.5f));
+            }
+            sphereShader.setMat4("model", modelorb);
+            glDrawArrays(GL_LINE_LOOP, 0, (GLsizei)orbVert.size() / 3);
+
+        }
+
+        modelorb = glm::mat4(1.0f);
+        modelorb = glm::translate(modelorb, posicaoPlanetas[2]);
+        modelorb = glm::scale(modelorb, glm::vec3(0.5f * 2.f, 0.5f * 2.f, 0.5f * 2.f));
+        sphereShader.setMat4("model", modelorb);
+        glDrawArrays(GL_LINE_LOOP, 0, (GLsizei)orbVert.size() / 3);
+
+
+		
         //###################################### SKYBOX ######################################
         glBindVertexArray(skyboxVAO);
 
@@ -465,22 +560,15 @@ int main()
         view = glm::mat4(glm::mat3(camera.GetViewMatrix())); // removemos a translacao da matriz view
         skyboxShader.setMat4("view", view);
         skyboxShader.setMat4("projection", projection);
-
-
         // skybox cube
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
         glDepthFunc(GL_LESS); // set depth function back to default
-
-   
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
-
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -503,6 +591,13 @@ void processInput(GLFWwindow* window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS)
+    {
+        camera.Position = glm::vec3(0.0f, 100.0f, -150.0f);
+        camera.Yaw = 90.0f;
+        camera.Pitch = -40.0f;
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -558,20 +653,13 @@ unsigned int loadTexture(char const* path)
             format = GL_RED;
         else if (nrComponents == 3)
             format = GL_RGB;
-
-
         glBindTexture(GL_TEXTURE_2D, textureID);
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-
-
-
         stbi_image_free(data);
     }
     else
